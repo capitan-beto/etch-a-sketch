d490da00d034f3cad9a55cc52c1c5d92f8dce0f0
Otroletravaladna <crnana98@gmail.com>
2022-09-30 03:09:25 -0300

    add sketch hover

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 let userInput = prompt("Type a number", "Max 100");
 let input = Number(userInput);
 
-
-function checkInput(input){
-    if (input > 100){
-        alert("Dumb-o! the max is 100");
-        document.location.reload();
-        return;
-    } else if (input === "  " || null || undefined){
-        alert("Type a num dumb dumb");
-        document.location.reload();
-        return;
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
     }
-}
+    return 'rgb(' + color.join(', ') + ')';
+}     
+
+
  
 function  setGrid(input){
     if (input > 100){
@@ -33,11 +30,19 @@ function  setGrid(input){
                 childDiv.classList.add("column");
                 childDiv.textContent = "";
                 parentDiv.appendChild(childDiv);
+                childDiv.addEventListener("mouseover", function(){
+                    childDiv.style.backgroundColor = randomColor();
+                    setInterval(() => {
+                        childDiv.setAttribute("style", "background-color: gray;")
+                    }, 4000/1);
+                });
+                
             } 
         }
     } else {
         document.location.reload();
     }
+
 }
 
 setGrid(input);
