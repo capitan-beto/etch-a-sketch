a627c942fa39c25517af214e3fba8ac4534f2e40
Otroletravaladna <crnana98@gmail.com>
2022-09-30 20:38:27 -0300

    Add reset B&W and reset Color butttons. Clean script code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let userInput = prompt("Type a number", "Max 100");
 let input = Number(userInput);
+let newInput = input;
 
 function randomColor() {
     let color = [];
@@ -8,15 +9,6 @@ function randomColor() {
     }
     return 'rgb(' + color.join(', ') + ')';
 }     
-
-function setOpacity(){
-    if(childDiv.style.Opacity = "rgba(0,0,0,0.1)"){
-        childDiv.style.Opacity = "rgba(0,0,0,0.3)"
-        console.log("workinggg");
-    }
-    
-}
-
 
  
 function  setGrid(input){
@@ -41,48 +33,28 @@ function  setGrid(input){
                 childDiv.addEventListener("mouseover", function(){
                     childDiv.style.backgroundColor = randomColor();
                 });
+                const bnwGrid = document.querySelector(".set-bnw");  
+                bnwGrid.addEventListener("click", () =>{
+                    childDiv.style.backgroundColor = "white";
+                    childDiv.addEventListener("mouseover", () =>{
+                            childDiv.style.backgroundColor = "rgba(0,0,0,1)";
+                        })
+                })
+                const colorGrid = document.querySelector(".set-color");
+                colorGrid.addEventListener("click", () => {
+                    childDiv.style.backgroundColor = "white";
+                    childDiv.addEventListener("mouseover", () => {
+                        childDiv.style.backgroundColor = randomColor();
+                        {once:true}
+                    })
+                })
             } 
         }
     } else {
         document.location.reload();
     }
-
 }
 
-//setGrid(input);
-
-function  setBlack(input){
-    if (input > 100){
-        alert("Dumb-o! the max is 100");
-        return;
-    } else if (input > 0 && input <= 100){     
-        const container = document.createElement("div");
-        container.classList.add("container");
-        document.body.appendChild(container);
-        for (let i = 0; i < input; i++){
-            const parentDiv = document.createElement("div");
-            parentDiv.classList.add("row");
-            parentDiv.textContent = "";
-            container.appendChild(parentDiv);
-
-            for (let i = 0; i < input; i++){
-                const childDiv = document.createElement("div");
-                childDiv.classList.add("column");
-                childDiv.textContent = "";
-                parentDiv.appendChild(childDiv);
-                childDiv.addEventListener("mouseover", function(e){
-                    childDiv.style.backgroundColor = "rgba(0,0,0,1)";
-                });
-            } 
-        }
-    } else {
-        document.location.reload();
-    }
-
-}
-
-setBlack(input);
-
-
+setGrid(input);
 
 
